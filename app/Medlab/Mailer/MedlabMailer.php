18621c7aef7e9b7344af3965cd95606e468541d4
Henry <henry.work@iinet.net.au>
2016-03-23 14:20:04 +1100

    change order received email to sales@medlab.co

--- app/Medlab/Mailer/MedlabMailer.php
+++ app/Medlab/Mailer/MedlabMailer.php
@@ -130,7 +130,7 @@ class MedlabMailer
     public function sendRegistrationReceivedNoticeToAdmin($registration)
     {
         $from = $this->AdminEmailAddress;
-        $to = $this->AdminEmailAddress;
+        $to = "sales@medlab.co";
 
         $this->mail->queue('emails.new_registration_received', compact('registration'), function($message) use ($registration, $from, $to) {
 
