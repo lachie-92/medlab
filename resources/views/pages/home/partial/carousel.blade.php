a7aa256c9f8898eafc4badfb368be7ea1b9b2f0e
Henry <henry.work@iinet.net.au>
2016-04-27 15:53:34 +1000

    new banner uploaded

--- resources/views/pages/home/partial/carousel.blade.php
+++ resources/views/pages/home/partial/carousel.blade.php
@@ -15,39 +15,44 @@
             <li data-target="#medlab-carousel" data-slide-to="5"></li>
             <li data-target="#medlab-carousel" data-slide-to="6"></li>
             <li data-target="#medlab-carousel" data-slide-to="7"></li>
+            <li data-target="#medlab-carousel" data-slide-to="8"></li>
         </ol>
 
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-                <img class="medlab_carousel_image" src="/img/carousel/1.jpg" alt="1">
+                <img class="medlab_carousel_image" src="/img/carousel/NOS_web_banner_final.jpg" alt="1">
                 <div class="carousel-caption"></div>
             </div>
             <div class="item">
-                <img class="medlab_carousel_image" src="/img/carousel/2.jpg" alt="2">
+                <img class="medlab_carousel_image" src="/img/carousel/1.jpg" alt="2">
                 <div class="carousel-caption"></div>
             </div>
             <div class="item">
-                <img class="medlab_carousel_image" src="/img/carousel/3.jpg" alt="3">
+                <img class="medlab_carousel_image" src="/img/carousel/2.jpg" alt="3">
                 <div class="carousel-caption"></div>
             </div>
             <div class="item">
-                <img class="medlab_carousel_image" src="/img/carousel/4.jpg" alt="4">
+                <img class="medlab_carousel_image" src="/img/carousel/3.jpg" alt="4">
                 <div class="carousel-caption"></div>
             </div>
             <div class="item">
-                <img class="medlab_carousel_image" src="/img/carousel/5.jpg" alt="5">
+                <img class="medlab_carousel_image" src="/img/carousel/4.jpg" alt="5">
                 <div class="carousel-caption"></div>
             </div>
             <div class="item">
-                <img class="medlab_carousel_image" src="/img/carousel/6.jpg" alt="6">
+                <img class="medlab_carousel_image" src="/img/carousel/5.jpg" alt="6">
                 <div class="carousel-caption"></div>
             </div>
             <div class="item">
-                <img class="medlab_carousel_image" src="/img/carousel/7.jpg" alt="7">
+                <img class="medlab_carousel_image" src="/img/carousel/6.jpg" alt="7">
                 <div class="carousel-caption"></div>
             </div>
             <div class="item">
-                <img class="medlab_carousel_image" src="/img/carousel/8.jpg" alt="7">
+                <img class="medlab_carousel_image" src="/img/carousel/7.jpg" alt="8">
+                <div class="carousel-caption"></div>
+            </div>
+            <div class="item">
+                <img class="medlab_carousel_image" src="/img/carousel/8.jpg" alt="9">
                 <div class="carousel-caption"></div>
             </div>
         </div>
