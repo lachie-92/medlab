24e2dcf9a2f4cbed0aff9ea4a776dad8236051d3
Henry <henry.work@iinet.net.au>
2016-05-30 15:09:46 +1000

    fix flue ypo

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -349,7 +349,7 @@ Route::get('/efficacy/role-of-bacteria-in-git-conditions', function () {
     return view('pages.efficacy.role-of-bacteria-in-git-conditions.index');
 });
 
-Route::get('/efficacy/optimising-immunity-in-preparation-for-cold-and-flue-season', function () {
+Route::get('/efficacy/optimising-immunity-in-preparation-for-cold-and-flu-season', function () {
     return view('pages.efficacy.optimising-immunity.index');
 });
 
--- resources/views/pages/efficacy/optimising-immunity/partial/breadcrumbs.blade.php
+++ resources/views/pages/efficacy/optimising-immunity/partial/breadcrumbs.blade.php
@@ -8,7 +8,7 @@
         <ol class="breadcrumb medlab_breadcrumbs">
             <li><a class="medlab_breadcrumbs_link" href="/">Home</a></li>
             <li><a class="medlab_breadcrumbs_link" href="/">Efficacy</a></li>
-            <li class="active medlab_breadcrumbs_text">Optimising Immunity in preparation for cold and flue season</li>
+            <li class="active medlab_breadcrumbs_text">Optimising Immunity in preparation for cold and flu season</li>
         </ol>
     </div>
 </div>
