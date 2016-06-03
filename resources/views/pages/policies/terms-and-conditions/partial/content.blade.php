3804e8f4432295cc0730653457befd0f55791f0e
LachlanWork <lachlan_young@medlab.co>
2016-06-03 16:13:56 +1000

    Privacy Policy

--- resources/views/pages/policies/terms-and-conditions/partial/content.blade.php
+++ resources/views/pages/policies/terms-and-conditions/partial/content.blade.php
@@ -142,19 +142,163 @@
                         product does not fall within the Returns Policy.
                     </p>
                     <p>
-                        <strong>(7) Privacy</strong>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>(7) Privacy</strong>
                     </p>
                     <p>
                         At Medlab Pty Ltd, we respect your privacy and we are committed to protecting your personal information, which is why we
                         abide to the National Privacy Principles that support the Australian Privacy Act 1988. Any personal information collected
                         by us through your use of this website will be dealt with in accordance with our Privacy Policy which may be accessed from
                         our home page.
-                    </p>
+                    </p><br>
                     <p>
-                        &nbsp;
-                    </p>
+                        <h4>In addition to the Terms above, the following Terms relate to specially to pre-approved arrangements where Consignment Stock has been provided:</h4><br>
+
+                    <h3>TERMS AND CONDITIONS – CONSIGNMENT STOCK</h3>
+
+                    <table style="width:100%">
+                        <col width=5%>
+                        <col width=95%>
+                        <tr>
+                            <td>1.</td>
+                            <td style="padding:10px">INTERPRETATION AND DEFINITIONS</td>
+                        </tr>
+                        <tr>
+                            <td>1.1.1</td>
+                            <td style="padding:7px">"Consignment Stock" shall mean Medlab branded Nutraceutical products that is being held by the Client for sales but remains the legal title of Medlab.</td>
+                        </tr>
+                        <tr>
+                            <td>1.1.2</td>
+                            <td style="padding:7px">"Intellectual Property Rights" shall mean in relation to the Consignment Stock (as hereinafter defined) any and all product licence and marketing authorisation applications and approvals, clinical authorisations, patents, supplementary protection certificates, continuations, continuations in part, divisions, patents of addition, reissues, renewals, extensions, petty patents, utility models, trade marks and approvals, service marks, trade or business names, logos, brand names, inventions, formulae, specifications, protocols, processes, designs, design rights, patterns, trade secrets, data, information, copyrights in technical materials, copyrights, database rights, domain names or other similar property rights thereto whether registered or unregistered and including applications for the same.</td>
+                        </tr>
+                        <tr>
+                            <td>2.</td>
+                            <td style="padding:10px">PURCHASE ORDERS: The Client shall submit purchase orders to Medlab in writing.  All purchase orders shall include:<br>
+
+                                (a) a description of and the quantity of each of the Consignment Stock to be ordered;<br>
+
+                                (b) the requested delivery date(s); and<br>
+
+                                (c) any other information dictated by the circumstances of the order.<br>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>3.</td>
+                            <td style="padding:10px">TITLE:  Ownership of, property in and title to any of the Consignment Stock remains with Medlab until such time as the Consignment Stock has been sold by the Client.  </td>
+                        </tr>
+                        <tr>
+                            <td>4.</td>
+                            <td style="padding:10px">PRODUCT ACCEPTANCE:  The Consignment Stock delivered by Medlab will be subject to inspection by the Client, and in the event any Consignment Stock are found to be defective in material or workmanship, the Client shall have the right to reject within five (5) days of receipt of the Consignment Stock by the Client. Consignment Stock rejected by the Client shall have a failure report prepared and forwarded to Medlab.  Medlab may request that samples of the allegedly defective Consignment Stock be sent to Medlab. On receipt Medlab will assess the suspect Consignment Stock and determine a course of action. If the Consignment Stock is determined to be not merchantable or of acceptable quality, Medlab will accept return of the damaged Consignment Stock. Each case will be assessed in isolation.  </td>
+                        </tr>
+                        <tr>
+                            <td>5.</td>
+                            <td style="padding:10px">PRICING</td>
+                        </tr>
+                        <tr>
+                            <td>5.1</td>
+                            <td style="padding:7px">PRODUCT PRICE. The price charged by Medlab to the Client will be Medlab's then current wholesale price less 10%.  Monthly specials run by Medlab will not apply to Consignment Stock.</td>
+                        </tr>
+                        <tr>
+                            <td>5.2</td>
+                            <td style="padding:7px">PRICE INCREASE.  Medlab has the right at any time to revise the wholesale prices of the Consignment Stock.</td>
+                        </tr>
+                        <tr>
+                            <td>5.3</td>
+                            <td style="padding:7px">TAXES, FEES AND DOCUMENTS.  All prices described herein are exclusive of all applicable federal, state and local excise, sales, use and similar taxes and duties (collectively referred to as "taxes").  The Client shall be liable for and shall pay any and all applicable taxes invoiced by Medlab. </td>
+                        </tr>
+                        <tr>
+                            <td>6.</td>
+                            <td style="padding:10px">PAYMENT:  Terms of Payment shall be due at the end of the month following from the date of invoice into the bank account nominated in writing by Medlab. Medlab factors an interest rate of 2% compounded monthly for all late payments.</td>
+                        </tr>
+                        <tr>
+                            <td>7.</td>
+                            <td style="padding:10px">OBLIGATIONS OF CLIENT</td>
+                        </tr>
+                        <tr>
+                            <td>7.1.</td>
+                            <td style="padding:7px">he Client is to maintain the Consignment Stock in a manner suitable for Medlab to accept return.  If the Consignment Stock is damaged in anyway (eg broken, opened, contains other labelling or too dirty to be sold), Medlab reserves the right to charge for the damaged Consignment Stock whether the Client has sold the Consignment Stock or not.</td>
+                        </tr>
+                        <tr>
+                            <td>7.2.</td>
+                            <td style="padding:7px">The Client will provide access (at least monthly) to an authorised representative of Medlab in order to gain access to the Consignment Stock and to conduct a physical count of the.  The Client will be issued with an invoice for any Consignment Stock that cannot be accounted for, or for any stock that has deemed to be unsuitable for return in accordance with Clause 7.1.  </td>
+                        </tr>
+                        <tr>
+                            <td>7.3.</td>
+                            <td style="padding:7px">Maintain good stock rotation policies.  New Consignment Stock is placed at the rear of the existing Consignment Stock (FIFO).</td>
+                        </tr>
+                        <tr>
+                            <td>7.4.</td>
+                            <td style="padding:7px">Inter-store/clinic relocation or transfer of Consignment Stock is not permitted and will trigger a sale being deemed.</td>
+                        </tr>
+                        <tr>
+                            <td>7.5.</td>
+                            <td style="padding:7px">Consignment Stock required to be maintained under special storage instructions (eg refrigerated) must be adhered to.</td>
+                        </tr>
+                        <tr>
+                            <td>8.</td>
+                            <td style="padding:10px">INVENTORY AND MINIMUM PURCHASES:   Both parties will mutually agree the products to be treated as Consignment Stock and to what inventory levels to be maintained. After the visit by the Medlab staff member, Consignment Stock levels may be amended as a result of discussions between the parties.</td>
+                        </tr>
+                        <tr>
+                            <td>9.</td>
+                            <td style="padding:10px">TERM AND TERMINATION</td>
+                        </tr>
+                        <tr>
+                            <td>9.1.</td>
+                            <td style="padding:7px">TERM.  The initial term of this Agreement shall be 6 months from the execution of this Agreement by the Client.  Thereafter, this Agreement shall – subject to the rights to terminate contained herein or at law – continue for successive periods of 6 months (from the anniversary date of execution by the Distributor). </td>
+                        </tr>
+                        <tr>
+                            <td>9.2.</td>
+                            <td style="padding:7px">NOTICE OF TERMINATION.  Either party may, without penalty, terminate this Agreement in writing with 30 days notice.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>9.3.</td>
+                            <td style="padding:7px">EFFECT OF TERMINATION OR EXPIRATION  </td>
+                        </tr>
+                        <tr>
+                            <td>9.4.1.</td>
+                            <td style="padding:7px"> Termination or expiration of this Agreement shall not relieve or release either party from making payments which may be owing to the other party under the terms of this Agreement. Consignment Stock in the possession of the Client as at the effective date of termination or expiration shall be returned to Medlab.</td>
+                        </tr>
+                        <tr>
+                            <td>9.4.2.</td>
+                            <td style="padding:7px">The Client acknowledges and accepts that it has no right to any compensation (howsoever desired) from Medlab where this Agreement is terminated by Medlab.</td>
+                        </tr>
+                        <tr>
+                            <td>10.</td>
+                            <td style="padding:10px">INTELLECTUAL PROPERTY RIGHTS: All Intellectual Property Rights relating to the Consignment Stock remain the the property of Medlab.</td>
+                        </tr>
+                        <tr>
+                            <td>11.</td>
+                            <td style="padding:10px">LIMITATION OF LIABILITY</td>
+                        </tr>
+                        <tr>
+                            <td>11.1.</td>
+                            <td style="padding:7px">LIABILITY LIMITED TO PURCHASE PRICE. To the fullest extent permitted at law, Medlab's liability in respect of any claim or dispute regarding any product sold to the Client shall never exceed the price paid by the distributor for such product. The Client shall indemnify and hold harmless Medlab from any claim or liability in excess of such price.</td>
+                        </tr>
+                        <tr>
+                            <td>11.2.</td>
+                            <td style="padding:7px">NO CONSEQUENTIAL DAMAGES.  Further, to the fullest extent permitted at law, in no event will Medlab be liable for the costs of procurement of substitute products or services, lost profits or any special, indirect, consequential or incidental damages howsoever caused or on any theory of liability arising in any way out of this agreement.  This limitation shall apply even if Medlab has been advised of the possibility of such damages and notwithstanding any failure of essential purpose of any limited remedy provided herein.</td>
+                        </tr>
+                        <tr>
+                            <td>12.</td>
+                            <td style="padding:10px">FACIMILE NUMBERS:<br>
+
+                                MEDLAB: 61 2 9699 3347</td>
+                        </tr>
+                        <tr>
+                            <td>13.</td>
+                            <td style="padding:10px">GOVERNING LAW AND JURISDICTION.  This Agreement takes effect under, is governed by and shall be construed in accordance with the laws of the State of New South Wales, Australia. </td>
+                        </tr>
+
+
+                    </table>
+
+
+
+
+
+
                     <p align="center">
-                        Updated and effective as at March 5, 2015
+                        Updated and effective as at June 3, 2016
                     </p>
                     <p>
                         &nbsp;
