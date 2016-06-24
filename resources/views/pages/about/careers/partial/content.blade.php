6076ed50fe1948b56ce61d451aff420fdba440e0
LachlanWork <lachlan_young@medlab.co>
2016-06-24 15:39:09 +1000

    Careers

--- resources/views/pages/about/careers/partial/content.blade.php
+++ resources/views/pages/about/careers/partial/content.blade.php
@@ -22,7 +22,11 @@
                     <hr>
                     <h4>Positions Available</h4>
                     <a href="#MarketingCoordinator">Marketing Coordinator</a></br>
-                    <a href="#PractitionerConsultant">Client Relationship Manager</a></br>
+                    <a href="#ClientRelationshipManager">Client Relationship Manager</a></br>
+                    <a href="#PractitionerConsultantRepNSW">Practitioner Consultant Field Rep New South Wales</a></br>
+                    <a href="#PractitionerConsultantRepSEQ">Practitioner Consultant Field Rep South East Queensland</a></br>
+                    <a href="#PractitionerConsultantVIC">Practitioner Consultant Field Rep Victoria</a></br>
+
 
                     <hr>
 
@@ -83,7 +87,7 @@
 
                     <hr>
 
-                    <a name="PractitionerConsultant"></a>
+                    <a name="ClientRelationshipManager"></a>
                     <h3>Client Relationship Manager</h3>
                     <li>Client Relationship Manager</li>
                     <li>Full Time / Permanent</li></br>
@@ -123,7 +127,116 @@
                     <li>On-going training, education and mentoring.</li>
                     <li>Casual dress code</li></br>
                     <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change…</p>
-                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Practitioner Consultant">recruit@medlab.co</a></p>
+                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Client Relationship Manager">recruit@medlab.co</a></p>
+                    <hr>
+
+
+                    <a name="PractitionerConsultantRepNSW"></a>
+                    <h3>Practitioner Consultant Field Rep New South Wales</h3>
+                    <li>Full Time / Permanent</li></br>
+                    <p>Due to a rapid increase in market share we are seeking a highly ambitious Practitioner  Consultant Field Rep to join our extremely motivated successful team, where we can provide you with ongoing career development within a rewarding and collaborative team culture.</p>
+
+                    <h4>The Opportunity</h4>
+                    <p>As a passionate Client Relationship Manager, you will act as the interface between our company and healthcare practitioners, As a passionate Practitioner Consultant Field Rep, you will act as the face between our company and healthcare practitioners, consultatively working to increase the visibility and awareness of Medlab's practitioner-only nutraceutical range within your territory whilst meeting and exceeding sales targets.</p>
+                    <p>The role will be a combination of business development over the phone, face to face appointments and product training to practitioners, therefore you must possess strong communication and relationship building skills, whilst not being afraid of a challenge.</p>
+                    <p>Duties and Responsibilities include:</p>
+                    <li>Continually identify and develop new business opportunities.</li>
+                    <li>Increase product sales in existing and new clients through building strong relationships and the ongoing education and training of healthcare practitioners, either over the phone or face to face.</li>
+                    <li>Execute a business plan for your territory inline with meeting and exceeding key performance indicators and territory targets.</li>
+                    <li>Create and implement client incentive and retention strategies.</li>
+                    <li>Ensure the highest level of product and technical knowledge is attained and maintained.</li>
+                    <li>Plan and deliver product training to a range of healthcare modalities.</li>
+                    <li>Attend industry and association conferences and trade shows when required.</li></br>
+
+                    <h4>About You</h4>
+                    <p>To be successful in this role you will be able to demonstrate the following:</p>
+                    <li>You will ideally hold a naturopathic or similar complementary medicine qualification or have a pharmacy, nursing or science related qualification.</li>
+                    <li>Possess high level of interpersonal skills, with the ability to motivate and influence, creating strong relationships within the practitioner field.</li>
+                    <li>Proven track record in a sales environment along with commercial acumen.</li>
+                    <li>Self motivated and have a solutions based approach to tasks.</li>
+                    <li>Excellent understanding of the sales process and use of CRM system.</li>
+                    <li>Have a genuine passion for health and wellness with desire to succeed.</li></br>
+
+                    <h4>Benefits</h4>
+                    <p>This is a fantastic opportunity to join a globally established business that has great plans for expansion. Here at Medlab we are committed to the development of staff and regularly promote from within.</p>
+                    <p>You will be joining a highly motivated team with an organisation who provides excellent employee benefits and incentives which include the following:</p>
+                    <li>Generous base salary plus commission structure and allowances.</li>
+                    <li>A fun, collaborative team-working environment.</li>
+                    <li>On-going training, education and mentoring.</li>
+                    <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change…</p>
+                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Practitioner Consultant Field Rep New South Wales">recruit@medlab.co</a></p>
+                    <hr>
+
+                    <a name="PractitionerConsultantRepSEQ"></a>
+                    <h3>Practitioner Consultant Field Rep South East Queensland</h3>
+                    <li>Full Time / Permanent</li></br>
+                    <p>Due to a rapid increase in market share we are seeking a highly ambitious Practitioner  Consultant Field Rep to join our extremely motivated successful team, where we can provide you with ongoing career development within a rewarding and collaborative team culture.</p>
+
+                    <h4>The Opportunity</h4>
+                    <p>As a passionate Client Relationship Manager, you will act as the interface between our company and healthcare practitioners, As a passionate Practitioner Consultant Field Rep, you will act as the face between our company and healthcare practitioners, consultatively working to increase the visibility and awareness of Medlab's practitioner-only nutraceutical range within your territory whilst meeting and exceeding sales targets.</p>
+                    <p>The role will be a combination of business development over the phone, face to face appointments and product training to practitioners, therefore you must possess strong communication and relationship building skills, whilst not being afraid of a challenge.</p>
+                    <p>Duties and Responsibilities include:</p>
+                    <li>Continually identify and develop new business opportunities.</li>
+                    <li>Increase product sales in existing and new clients through building strong relationships and the ongoing education and training of healthcare practitioners, either over the phone or face to face.</li>
+                    <li>Execute a business plan for your territory inline with meeting and exceeding key performance indicators and territory targets.</li>
+                    <li>Create and implement client incentive and retention strategies.</li>
+                    <li>Ensure the highest level of product and technical knowledge is attained and maintained.</li>
+                    <li>Plan and deliver product training to a range of healthcare modalities.</li>
+                    <li>Attend industry and association conferences and trade shows when required.</li></br>
+
+                    <h4>About You</h4>
+                    <p>To be successful in this role you will be able to demonstrate the following:</p>
+                    <li>You will ideally hold a naturopathic or similar complementary medicine qualification or have a pharmacy, nursing or science related qualification.</li>
+                    <li>Possess high level of interpersonal skills, with the ability to motivate and influence, creating strong relationships within the practitioner field.</li>
+                    <li>Proven track record in a sales environment along with commercial acumen.</li>
+                    <li>Self motivated and have a solutions based approach to tasks.</li>
+                    <li>Excellent understanding of the sales process and use of CRM system.</li>
+                    <li>Have a genuine passion for health and wellness with desire to succeed.</li></br>
+
+                    <h4>Benefits</h4>
+                    <p>This is a fantastic opportunity to join a globally established business that has great plans for expansion. Here at Medlab we are committed to the development of staff and regularly promote from within.</p>
+                    <p>You will be joining a highly motivated team with an organisation who provides excellent employee benefits and incentives which include the following:</p>
+                    <li>Generous base salary plus commission structure and allowances.</li>
+                    <li>A fun, collaborative team-working environment.</li>
+                    <li>On-going training, education and mentoring.</li>
+                    <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change…</p>
+                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Practitioner Consultant Field Rep South East Queensland">recruit@medlab.co</a></p>
+                    <hr>
+
+                    <a name="PractitionerConsultantRepVIC"></a>
+                    <h3>Practitioner Consultant Field Rep Victoria</h3>
+                    <li>Full Time / Permanent</li></br>
+                    <p>Due to a rapid increase in market share we are seeking a highly ambitious Practitioner  Consultant Field Rep to join our extremely motivated successful team, where we can provide you with ongoing career development within a rewarding and collaborative team culture.</p>
+
+                    <h4>The Opportunity</h4>
+                    <p>As a passionate Client Relationship Manager, you will act as the interface between our company and healthcare practitioners, As a passionate Practitioner Consultant Field Rep, you will act as the face between our company and healthcare practitioners, consultatively working to increase the visibility and awareness of Medlab's practitioner-only nutraceutical range within your territory whilst meeting and exceeding sales targets.</p>
+                    <p>The role will be a combination of business development over the phone, face to face appointments and product training to practitioners, therefore you must possess strong communication and relationship building skills, whilst not being afraid of a challenge.</p>
+                    <p>Duties and Responsibilities include:</p>
+                    <li>Continually identify and develop new business opportunities.</li>
+                    <li>Increase product sales in existing and new clients through building strong relationships and the ongoing education and training of healthcare practitioners, either over the phone or face to face.</li>
+                    <li>Execute a business plan for your territory inline with meeting and exceeding key performance indicators and territory targets.</li>
+                    <li>Create and implement client incentive and retention strategies.</li>
+                    <li>Ensure the highest level of product and technical knowledge is attained and maintained.</li>
+                    <li>Plan and deliver product training to a range of healthcare modalities.</li>
+                    <li>Attend industry and association conferences and trade shows when required.</li></br>
+
+                    <h4>About You</h4>
+                    <p>To be successful in this role you will be able to demonstrate the following:</p>
+                    <li>You will ideally hold a naturopathic or similar complementary medicine qualification or have a pharmacy, nursing or science related qualification.</li>
+                    <li>Possess high level of interpersonal skills, with the ability to motivate and influence, creating strong relationships within the practitioner field.</li>
+                    <li>Proven track record in a sales environment along with commercial acumen.</li>
+                    <li>Self motivated and have a solutions based approach to tasks.</li>
+                    <li>Excellent understanding of the sales process and use of CRM system.</li>
+                    <li>Have a genuine passion for health and wellness with desire to succeed.</li></br>
+
+                    <h4>Benefits</h4>
+                    <p>This is a fantastic opportunity to join a globally established business that has great plans for expansion. Here at Medlab we are committed to the development of staff and regularly promote from within.</p>
+                    <p>You will be joining a highly motivated team with an organisation who provides excellent employee benefits and incentives which include the following:</p>
+                    <li>Generous base salary plus commission structure and allowances.</li>
+                    <li>A fun, collaborative team-working environment.</li>
+                    <li>On-going training, education and mentoring.</li>
+                    <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change…</p>
+                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Practitioner Consultant Field Rep Victoria">recruit@medlab.co</a></p>
                     <hr>
 
                 </div>
