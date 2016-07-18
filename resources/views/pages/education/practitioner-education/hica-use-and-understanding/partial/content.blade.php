17136f0cd47654984889cbcc8839ed26f019eb63
LachlanWork <lachlan_young@medlab.co>
2016-07-19 08:48:31 +1000

    Rica Use Article Redo

--- resources/views/pages/education/practitioner-education/hica-use-and-understanding/partial/content.blade.php
+++ resources/views/pages/education/practitioner-education/hica-use-and-understanding/partial/content.blade.php
@@ -19,72 +19,39 @@
                     </h3>
 
                     <p>
-                        A French microbiologist, Henri Boulard discovered the beneficial yeast <em>Saccharomyces boulardii</em>
-                        in 1920. When he was visiting IndoChina he noticed that certain people who drank a special tea did
-                        not develop cholera during an outbreak. He isolated the agent in the fruit tea responsible for the
-                        therapeutic effects, being S. boulardii.<sup>1</sup> <em>S. boulardii</em> has several different
-                        mechanisms of action including luminal action, trophic action and mucosal anti-inflammatory
-                        signalling effects. Within the intestinal lumen, <em>S. boulardii</em> may interfere with
-                        pathogenic toxins, preserve cellular physiology, interfere with pathogen attachment, interact
-                        with normal microbiota and assist in re-establishing short chain fatty acid levels.
-                        <em>S. boulardii </em>may also act as an immune regulator, both within the lumen and systemically.
-                        Factors that disrupt the normal barrier function of commensal bacteria, such as antibiotic use or
-                        surgery, results in host susceptibility to pathogen colonization until the normal microbiota can
-                        become re-established. Typically it takes six to eight weeks for normal microbiota to recover after
-                        antibiotic use but when <em>S. boulardii</em> is supplemented, normal microbiota is re-established
-                        rapidly.<sup>1</sup>Furthermore, <em>S. boulardii</em> has proven effective in treating
-                        antibiotic-associated diarrhoea, <em>Clostridium difficile</em> infections, reducing side-effects
-                        of triple therapy to treat <em>Helicobacter pylori</em>, acute diarrhoea, travellers diarrhoea,
-                        Irritable Bowel Syndrome (IBS) and inflammatory bowel disease (IBD). <em>S. boulardii</em> has
-                        also been assessed for efficacy in treating <em>Giardiasis</em> when combined with metronidazole
-                        resulting in significantly less detection of <em>giardia </em>cysts compared to placebo.<sup>1-3</sup>
+                        A significant body of research has been conducted demonstrating digestive and colonic health
+                        benefits of High Amylose Maize Starch (HAMS) with research also showing metabolic advantages.<sup>1</sup>
+                        Resistant starch is the total amount of starch, and the products of starch degradation that
+                        resists digestion in the small intestine. Hi-Maize demonstrates numerous health benefits
+                        such as increasing satiety and helping with weight control, assisting with glycaemic
+                        management and promoting digestive health. Hi-Maize demonstrates increased satiety compared
+                        to other forms of fibre helping people to feel fuller in the hours after consuming it<sup>2</sup> and
+                        has also shown to help people eat less after 2 hours<sup>3</sup> as well over the proceeding 24 hours.<sup>4</sup>
+                        Hi-Maize consumption has been shown to increase insulin sensitivity in healthy people,<sup>5,6</sup>
+                        in people with type 2 diabetes<sup>7</sup> and in individuals with insulin resistance.<sup>8-10</sup> Furthermore,
+                        as a prebiotic, Hi-Maize supports beneficial bacterial growth and activity, increasing
+                        short-chain fatty acid production and  assisting with normal intestinal function.<sup>11-13</sup>
                     </p>
                     <p>
-                        Arabinogalactans are high molecular weight, highly branched, water-soluble polysaccharides
-                        containing units of D-galactose and L-arabinose. Arabinogalactans from Larch have been shown to
-                        stimulate natural killer cell cytotoxicity <em>in vitro</em> with animal studies demonstrating
-                        increases in white blood cell counts i.e. neutrophils and eosinophil’s.<sup>4</sup> ResistAid&trade;
-                        is a proprietary arabinogalactan extracted from Larch (<em>Larix laricina</em>) supplied by Lonza
-                        Ltd, Switzerland. A number of human clinical studies have demonstrated ResistAid&#39;s&trade;
-                        immunomodulatory activity. Udani 2010 demonstrated a daily dose of 4.5 g significantly increased
-                        IgG antibody response of healthy volunteers to the pneumococcal vaccine when compared to
-                        placebo.<sup>4</sup> In a more recent study Udani 2013 demonstrated that ResistAid&trade; at a dose
-                        of 1.5 g daily significantly increased the IgG antibody response to tetanus vaccine compared to
-                        placebo.<sup>5</sup> ResistAid&trade; has clinical evidence of being well tolerated in healthy
-                        individuals with no demonstrable side-effects.<sup>6</sup>
+                        Leucine has a unique role in metabolic regulation beyond the fundamental role of being an
+                        amino acid, a substrate for the synthesis of proteins. Leucine has a direct link to the
+                        maintenance of glucose homeostasis by enhancing the recycling of glucose via the glucose-alanine
+                        cycle and stimulates muscle protein synthesis through the insulin-signaling cascade during
+                        catabolic periods such as fasting or energy restriction.<sup>14,15</sup> Further, leucine supplementation
+                        stimulates recovery of muscle protein synthesis after exercise. The mechanisms of leucine
+                        appear to contribute to a metabolic advantage during weight loss.<sup>15</sup> Acetyl-L-Carnitine is
+                        crucial for beta-oxidation, facilitating the transport of long-chain fatty acids across the
+                        mitochondrial membrane for oxidation. When combined with weight loss motivation, the beneficial
+                        effects of L-Carnitine are amplified including significant body weight loss and a decrement of
+                        serum triglyceride levels when compared to participants supplementing with L-Carnitine but
+                        being assigned to a non-motived group for weight loss.<sup>16</sup> DL-α-hydroxy isocaproic acid (HICA)
+                        is an end product of leucine metabolism in human tissues such as muscle and connective tissue
+                        and is considered an anti-catabolic substance. HICA has demonstrated to increase whole lean
+                        body mass in healthy athletes and also decreased whole body delayed-onset muscle soreness
+                        (DOMS) symptoms.<sup>17</sup> The Medlab Lactobacilli plantarum (Med 25) and Lactobacillus paracasei
+                        (Med 24) have demonstrated the highest production of HICA when co-cultured with leucine.
                     </p>
-                    <p>
-                        Glutamine is the most abundant amino acid in the body and plays a central role in inter-organ
-                        carbon and nitrogen flux. Glutamine is considered a non-essential amino acid because it is
-                        synthesized endogenously, however it can be depleted rapidly in certain clinical settings
-                        particularly in response to injury, infection, IBD and short bowel syndrome.<sup>7,8</sup>
-                        Glutamine is an important fuel for rapidly dividing cells such as enterocytes, colonocytes and
-                        immune cells as it is critical for the biosynthesis of nucleotides required for cell replication
-                        and hexosamines. Glutamine and nucleosides act synergistically to promote differentiation and
-                        proliferation of intestinal epithelial cells.<sup>8</sup> Furthermore, glucosamine is reported to
-                        enhance the immunological barrier due to its trophic action on the immune system and its ability
-                        to decreased bacterial translocation.<sup>7</sup>
-                    </p>
-                    <p>
-                        Peppermint leaf and peppermint oil have traditionally been considered to have anti-spasmodic,
-                        anti-emetic, carminative and anti-microbial properties. Peppermint is indicated to treat a
-                        variety of digestive disorders such as flatulence, diarrhoea, indigestion, nausea and vomiting,
-                        and as a spasmolytic to reduce gas and cramping.<sup>9</sup> Peppermint is reported to block
-                        calcium channels resulting in gastrointestinal smooth muscle relaxation with a 2008 meta-analysis
-                        supporting the use of peppermint for providing short-term relief of discomfort and abdominal pain
-                        in IBS patients.<sup>10</sup>
-                    </p>
-                    <p>
-                        Cocoa is naturally rich in polyphenols that have been reported to modulate the gastrointestinal
-                        tract. Plasma and urine of chocolate eaters led to the discovery of distinct metabolic profiles
-                        according to the behavioural/psychological dietary preference for chocolate products. The
-                        metabolism of cocoa polyphenols can induce selective bacterial growth, i.e.
-                        <em>Bifidobacterium spp</em>, <em>Clostridium coccoides-Eubacterium rectale</em> group
-                        and <em>Escherichia coli</em> and significantly inhibit the growth of the
-                        <em>Clostridium histolyticum</em> group. Further, bacterial fermentation of the insoluble
-                        material of cocoa is associated with an increase in <em>Lactobacilli</em> and
-                        <em>Bifidobacterium</em> species as well as butyrate production.<sup>11</sup>
-                    </p>
+
 
                     <br>
                     <h4>
@@ -93,10 +60,12 @@
 
                     <p>
                     <ul>
-                        <li>Support healthy gastrointestinal tract function and physiology </li>
-                        <li>Help maintain healthy digestive function and relieve gastrointestinal  discomfort </li>
-                        <li>Support optimal immune function</li>
-                        <li>Relieve symptoms of medically diagnosed Irritable Bowel Syndrome</li>
+                        <li>Increase satiety and assist with weight management<sup>4</sup></li>
+                        <li>Modulate carbohydrate and lipid metabolism<sup>2,5</sup></li>
+                        <li>Promote digestive health<sup>6,7</sup></li>
+                        <li>Regulate blood-glucose levels & skeletal muscle protein synthesis in  healthy individuals<sup>1,2</sup></li>
+                        <li>Promote HICA synthesis indicated to increase lean muscle mass<sup>3</sup></li>
+                        <li>Modulate energy expenditure in healthy individuals<sup>8</sup></li>
                     </ul>
                     </p>
 
@@ -105,31 +74,32 @@
                     </p>
 
                     <p>
-                        <ul>
-                            <li>
-                                Those requiring relief from gastrointestinal complaints such as abdominal pain and
-                                bloating, flatulence, diarrhoea and/or constipation
-                            </li>
-                            <li>Those with intestinal permeability</li>
-                            <li>Those requiring prebiotic support for gastrointestinal microbial health</li>
-                        </ul>
+                    <ul>
+                        <li>Those seeking weight management support and lean body mass support</li>
+                    </ul>
                     </p>
 
                     <hr>
                     <strong>References</strong><br>
                     <br>
                         <ol class="medlab_product_reference_list">
-                            <li>McFarland L. Systematic review and meta-analysis of Saccharomyces boulardii in adult patients. <em>World J Gastroenterol</em>2010;16:2202. </li>
-                            <li>Pineton de Chambrun G, et al. A randomised clinical trial with Saccharomyces cerevisiae versus placebo in Irritable Bowel Syndrome. <em>Dig and Liver Dis</em> 2015;47:119.</li>
-                            <li>Kelesidis T, Pothoulakis C. Efficacy and safety of the probiotic Saccharomyces boulardii for the prevention and therapy of gastrointestinal disorders. <em>Therap Adv Gastroenterol</em> 2012;5:111. </li>
-                            <li>Udani JK, et al. Proprietary arabinogalactan extract increases antibody response to the pneumonia vaccine: a randomized double-blind, placebo-controlled pilot  study in health volunteers. <em>Nutr J</em> 2010;9:32.</li>
-                            <li>Udani JK. Immunomodulatory effects of ResistAid<sup>TM</sup>: A randomized, double-blind, placebo-controlled, multi dose study. <em>J Am Coll Nutr</em> 2013;32:331. </li>
-                            <li>Grube B, et al. Tolerability of a Proprietary Larch Arabinogalactan Extract: a randomized, double-blind, placebo-controlled clinical trial in healthy subjects. <em>Food Nutr Sci</em> 2012;3:1533.</li>
-                            <li>Garcia-de-Lorenzo A, et al. Clinical Evidence for the Enteral Nutritional Support with Glutamine: A Systematic Review. <em>Nutrition</em> 2003;19:9. </li>
-                            <li>Neu J, et al. Intestinal mucosal defense system, Part 1. Consensus recommendation for immunonutrients. <em>J Pediatr</em> 2013;162:S56.</li>
-                            <li>Gardiner P. Peppermint (Mentha piperita). <em>Longwood Herbal Task Force </em>2000;1-22.  </li>
-                            <li>Wall GC, et al. Irritable bowel syndrome: a concise review of current treatment concepts. <em>World J Gastroenterol</em> 2014;20:8796.</li>
-                            <li>Moco S, etr al. Metabolomics view on gut microbiome modulation by polyphenol rich foods. <em>J Proteome Res</em> 2012;11:4781.</li>
+                                        <li>Nugent AP. Health properties of Resistant Starch, British Nutrition Foundation. <em>NutrBull</em> 2005;30:27-54.</li>
+                                        <li>Willis HJ, et al. Greater satiety response with resistant starch and corn bran in human subjects. <em>Nutr Res</em> 2009;29:100-105.</li>
+                                        <li>Anderson GH, et al. Relation between estimates of corn starch digestibility by the Englyst in vitro method and glycemic response, subjective appetite and short-term food intake in young men. <em>Am J Clin Nutr</em> 2010;91:932-939.</li>
+                                        <li>Bodhinham CL, et al. Acute ingestion of resistant starch reduces food intake in healthy adults. Br J Nutr 2010;103:917-922.</li>
+                                        <li>Robertson DM, et al. Prior short-term consumption of resistant starch enhances postprandial insulin sensitivity insulin sensitivity in healthy subjects. <em>Diabetologia</em> 2003;46:659-665.</li>
+                                        <li>Robertson DM, et al. Insulin-sensitizing effects of resistant starch and effects on skeletal muscle and adipose tissue metabolism. <em>AM J Clin Nutr</em> 2005;82:559-567.</li>
+                                        <li>Zhang WQ, et al. Effects of resistant starch on insulin resistance of type 2 diabetes mellitus patients. <em>Chin J Prevent Med</em> 2007;41:101-104.</li>
+                                        <li>Johnson KL, et al. Resistant starch  improves insulin sensitivity in metabolic syndrome. <em>Diabetic Med</em> 2010;27:391-397.</li>
+                                        <li>Maki KC, et al. Resistant starch from high-amylose maize increases insulin sensitivity in overweight and obese men. <em>J Nutr</em> 2012;142:717-723.</li>
+                                        <li>Robertson MD, et al. Insulin-sensitizing effects on muscle and adipose tissue after dietary fiber intake in men and women with metabolic syndrome. <em>J Clin Endocrinol Metab</em> 2012;97:3326-32.</li>
+                                        <li>Brown I, et al. Fecal numbers of Bifidobacteria are higher in pigs fed Bifidobacterium longum with a high amylose cornstarch than with a low amylose cornstarch. <em>J Nutr</em> 1997;127:1822-1827.</li>
+                                        <li>Martinez I, et al. Resistant starch types 2 and 4 have differential effects on the composition of the fecal microbiota in human subjects. <em>PLoS ONE</em> 2010;5:e15046.</li>
+                                        <li>Ramakrishna BS, et al. Amylose-resistant starch plus oral rehydration solution for cholera. <em>NEJM</em> 2000;342:308-313.</li>
+                                        <li>Layman DK. Role of leucine in weight loss diets and glucose homeostasis. <em>J NUtr</em> 2003;133:261S-267S.</li>
+                                        <li>Layman DK. Protein quantity and quality at levels above the RDA improves adult weight loss. <em>J Am Coll Nutr</em> 2004;23:631-S-636S.</li>
+                                        <li>Odo S, et al. A pilot clinical trial on L-Carnitine supplementation in combination with motivation training: effects on weight management in healthy volunteers. <em>Food Nutr Sci</em> 2013;4:222-231.</li>
+                                        <li>Mero AA, et al. Effects of alfa-hydroxy-isocaproic acid on body composition, DOMS and performance in athletes. <em>J Int Soc Sports Nutr</em> 2010;7:1. </li>
                         </ol>
                     <hr>
 
