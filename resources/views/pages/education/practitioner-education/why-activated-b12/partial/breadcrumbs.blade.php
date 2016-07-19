9939a33473b7f8f77836e9baf44af1fcfa646a32
LachlanWork <lachlan_young@medlab.co>
2016-07-19 13:39:25 +1000

    Breadcrumbs

--- resources/views/pages/education/practitioner-education/why-activated-b12/partial/breadcrumbs.blade.php
+++ resources/views/pages/education/practitioner-education/why-activated-b12/partial/breadcrumbs.blade.php
@@ -8,7 +8,7 @@
         <ol class="breadcrumb medlab_breadcrumbs">
             <li><a class="medlab_breadcrumbs_link" href="/">Home</a></li>
             <li><a class="medlab_breadcrumbs_link" href="/">Education</a></li>
-            <li><a class="medlab_breadcrumbs_link" href="/">Practitioner Education</a></li>
+            <li><a class="medlab_breadcrumbs_link" href="/education/practitioner-education">Practitioner Education</a></li>
             <li class="active medlab_breadcrumbs_text">Why an Activated Form of B12</li>
         </ol>
     </div>
