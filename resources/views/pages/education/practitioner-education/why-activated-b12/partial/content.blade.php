8d410bf8177d640dd9de19165c464f84ce2e2408
LachlanWork <lachlan_young@medlab.co>
2016-08-22 15:20:09 +1000

    Activated B12

--- resources/views/pages/education/practitioner-education/why-activated-b12/partial/content.blade.php
+++ resources/views/pages/education/practitioner-education/why-activated-b12/partial/content.blade.php
@@ -18,76 +18,44 @@
                         <strong>WHY AN ACTIVATED FORM OF B12?</strong>
                     </h3>
 
-                    <p>
-                    All tissues have a requisite for uridine. Uridine is an important nucleoside precursor in the pyrimidine salvage pathway.<sup>8,9,10</sup> 
-                    Except for erythrocytes and the liver and kidney, which maintain de novo pyrimidine biosynthesis and supply other tissues with
-                    uridine for salvage, most normal tissues in adults rely on the salvage of uridine from plasma.<sup>9</sup>
-                    </p>
+                    <p>NanoCelle&trade; Activated B12 may assist in the management of dietary vitamin B12 deficiency as may occur in vegetarians, vegans, the elderly, and those taking certain medications that lower stomach acid. Methylcobalamin is a biologically active form of vitamin B12 that is involved in the methylation of homocysteine to methionine, therefore helping to lower serum homocysteine levels. Vitamin B12 is required for red blood cell production, synthesis of myelin that is vital to healthy neurological function and DNA methylation. NanoCelle@trade; delivers nanoparticle-sized molecules of the vitamin for absorption across the oral buccal mucosa, bypassing the gastrointestinal tract and greatly enhancing nutrient absorption with superior bioavailability.</p>
+
+                    <p>Vitamin B12 is one of the most complex vitamins in regards to its absorption and transport within the body and depends critically on a very tightly regulated transport system. Vitamin B12 is continually partnered with binding proteins throughout the digestive tract and blood stream and then requires a binder specific cellular receptor for intracellular transport. Intrinsic factor (IF), which is released from the stomach and is required to mediate transport of B12 into enterocytes in the ileum, is saturable, the rate at which varies inversely with the amount of B12 ingested. The IF-mediated transport rarely allows more than 1 to 2 g of B12 to be absorbed at any one time, therefore high doses quickly exceed the absorptive capacity of the body, with unabsorbed B12 being excreted in the faeces. Nonspecific and linear non-saturable diffusion does occur, in which a receptor is not required, however this fraction is extremely small and estimated to be approximately 1%.<sup>1</sup></p>
+
+                    <strong>NanoCelle™ Delivery of Vitamin B12</strong>
+
+                    <p>A novel way to bypass the limited absorptive capacity of vitamin B12 in the gastrointestinal tract (GIT) is to utilize the rich blood supply of the buccal mucosa. The buccal mucosa is non-keratinized and is therefore more permeable to water-soluble particles than keratinized epithelium such as the hard palate. The buccal mucosa is 40-50 cell layers thick, and compared to intestinal cells, may also have less tight junctions but a higher level of lipid intercellular matrix.<sup>1</sup> Once the NanoCelle@trade; molecule diffuses through the mucus layer and aqueous boundary layer on the surface of the epithelium, it may then transverse across the epithelium via passive diffusion (See Figure 1).<sup>2</sup> There are no B12 receptors on oral epithelium cells i.e. such as the cubam receptor on ileum enterocytes that facilitate the entry of IF-bound cobalamin into the enterocytes for transport into the blood stream.</p>
 
                     <p>
-                    Uridine also participates in the regulation of several physiological and pathological processes.<sup>11</sup> In the absence of sugar,
-                    uridine can serve as an essential precursor for both carbohydrate metabolism and nucleic acid synthesis. Besides the direct
-                    salvage to pyrimidine nucleotides, the ribose moiety produced by the phosphorolysis of uridine can either enter the pentose
-                    cycle to meet cellular energy requirements or form phosphoribosyl pyrophosphate, which is used in the biosynthesis of purine
-                    nucleotides.<sup>11</sup> In the nervous system, uridine acts as a physiological regulator of sleep function and demonstrates the ability
-                    to maintain brain metabolism during ischemia and severe hypoglycemia.
+                        <img class="img-responsive center-block" src="/img/practitioner-education/Act B12_figure1.png" alt="">
                     </p>
 
-                    <p>
-                    Coenzyme Q10 (CoQ10) is a compound that is naturally found intracellularly and bound to organelle membranes such as for example
-                    the mitochondria, endoplasmic reticulum, and golgi apparatus. CoQ10 is found with the highest concentrations in tissues such as
-                    the heart, liver, kidneys, and pancreas. This compound is intricate to the production of adenosine triphosphate (ATP). ATP is
-                    essential to all bodily functions, including the generation of energy and a multitude of metabolic functions.<sup>12,13,14</sup>
-                    </p>
+                    <p><strong>Figure 1. Oro-Buccal Nutrient Delivery.</strong> NanoCelle@trade; technology creates water-soluble particles that penetrate the buccal mucosal membrane via paracellular transport allowing direct entry into the blood stream. The buccal mucosa has a rich supply of blood vessels.<sup>3</sup>
+
+                    <strong>Function of Methylcobalamin</strong>
+
+                    <p>Vitamin B12 (Cobalamin – Cbl) exists in two active forms, methylcobalamin (MeCbl) and adenosylcobalamin (AdoCbl) that participate in specific physiological processes in the cell. MeCbl is a cofactor for the cytosolic methionine synthase enzyme and AdoCbl is the cofactor for the mitochondrial methylmalonyl-CoA mutase. In the cytosol, MeCbl is involved in transferring its methyl group to homocysteine to form methionine, which is part of the S-adenosyl methionine cycle. Elevated serum homocysteine levels are associated with increased risk of heart attack, stroke, blood clots and cognitive impairment.<sup>4,5</sup> AdoCbl is a key component of the tricarboxylic acid (TCA) pathway of carbohydrate metabolism, catalyzing the isomerization of methylmalonyl-CoA to succinyl-CoA resulting in ATP generation in cells (see Figure 2).<sup>6,7</sup> Methionine is required for the synthesis of myelin, therefore MeCbl is required for healthy nerve function. Further, while AdoCbl and CNCbl can increase DNA-methylation by methyltransferase, MeCbl is the most efficient compound.<sup>6</sup>
 
                     <p>
-                    Over the past several decades, research has seen a refocus towards thinking and efforts directed in elucidating the critical
-                    inter–relationships that exist between the GIT microbiome and its host. This research has redefined the interactions between
-                    gut microbes and vertebrates, now recognising that the microbial active cohort and its mammalian host have shared co–evolutionary
-                    metabolic interactions that span millennia. Microbial interactions in the GIT provide the necessary cues for the development of
-                    regulated pro– and anti–inflammatory signals that promotes immunological tolerance, metabolic regulation and other factors which
-                    may then control local and extra–intestinal inflammation.<sup>15</sup> Furthermore, it is also becoming apparent that the GIT with its commensal
-                    cohort is a central regulator for the activities of end organs such as the kidneys, brain, adipose tissue, muscle and liver and as
-                    such may provide local prompts that are transmitted extra–intestinally to end organ sites. A scientific insight therefore has
-                    emerged that plausibly links the GIT with the physiology of end organ function that may influence health maintenance or trigger
-                    and support a disease state. Maintaining a healthy GIT milieu and epithelium with the administration of probiotics may constitute
-                    a novel therapeutic strategy for health.
+                        <img class="img-responsive center-block" src="/img/practitioner-education/NanoCelle B12 Scientific Diagram_web.jpg" alt="">
                     </p>
 
-                    <p>
-                    Probiotics are those live bacterial cultures that are added to foods (e.g., yoghurts) and employed as dietary supplements, that
-                    when orally administered can improve the health of the host beyond their fundamental basic nutritional content.<sup>16</sup> Probiotic bacteria
-                    encompass those from different genera (as for example Lactobacilli, Bifidobacterium, Escherichia, Saccharomyces (a yeast),
-                    Streptococcus) giving rise to a variety of different species of each genera (i.e., Lactobacillus acidophilus; Lactobacillus
-                    bulgaricus, Lactobacillus rhamnosus); that lead to different strains within a species (i.e., Lactobacillus acidophilus La–1,
-                    Lactobacillus acidophilus NCFM). This taxonomic differentiation, importantly emphasizing that different strains from the same
-                    bacterial species can be different and may elaborate different physiological functions within the GIT<sup>17</sup> whilst exhibiting
-                    overlapping or specific therapeutic actions to different organ systems.<sup>18</sup>
-                    </p>
+                    <p>Figure 2. Methylcobalamin acts in the cytoplasm where it is involved in the Methionine cycle and Adenosylcobalamin in the mitochondria where it is involved in the Krebs cycle.<sup>7</sup></p>
+
+                    <p>Both hydroxy- (HOCbl) and cyanocobalamin (CNCbl) are able to convert to both the methyl- and adenosylcobalamin forms. It has been shown that this conversion can occur just after supplementation in the blood stream. There are some rare genetic conditions in which cyanocobalamin cannot be converted to the active forms.<sup>6,7</sup> It is not known if methylcobalamin or adenosylcobalamin supplementation results in these active forms reaching the cell intact and whether they are able to directly participate in their physiological roles. It has been recently suggested that the trafficking and assimilation of all forms of cobalamin (MeCbl, AdoCbl, CNCbl and HOCbl) likely follow the same metabolic steps in the body: cross the intestinal/mucosal barrier, bind to transcobalamin, undergo cellular internalization and lysosomal processing. Methylmalonic aciduria and homocystinuria type C protein (MMACHC) in the cytosol then removes the upper ligand’s of Cbl via dealkylation (MeCbl, AdoCbl), decyanation (CnCbl) or reduction (CNCbl, HOCbl) preparing Cbl for being methylated or adenosylated, converting it into the coenzyme forms.<sup>6,7</sup> All forms of Cbl appear to be absorbed and efficient in treating vitamin B12 deficiency</p>
 
 
                     <hr>
                     <strong>References</strong><br>
                     <br>
                     <ol class="medlab_product_reference_list">
-                        <li>Crane F. Biochemical functions of CoQ10. <em>J Am Coll Nutr 2001;20:591-598.</em>
-                        <li>Kon M et al. Reducing exercise-induced muscular injury in Kendo athletes with supplementation of CoQ10. <em>Br Nutr2008;100:903-909.</em>
-                        <li>Giuseppe C et al. Effect of CoQ10 on myopathic symptoms in patients treated with statins. <em>Am J Cardiol 2007;99:1409-1412.</em>
-                        <li>Ishizuka A et al. Effects of administration of Bifidobacterium animalis ssp. lactis GCL2505 on defecation frequency and bifidobacterial microbiota composition in humans. <em>J Biosci Bioeng 2012;113:587-91.</em>
-                        <li>de Vrese M & Schrezenmeir J. Probiotics, prebiotics, and synbiotics. <em>Adv Biochem Eng Biotechnol 2008;111:1-66.</em>
-                        <li>Agarwal R et al. Pathogenic role of magnesium deficiency in ophthalmic diseases. <em>Biometals 2013 [Epub ahead of print].</em>
-                        <li>Dongarra L et al. Mucosal immunology and probiotics. <em>Curr Allergy Asthma Rep 2013;13:19-26.</em>
-                        <li>Shambaugh GE. Pyrimidine biosynthesis. <em>AJCN 1979;32:1290 –1297.</em>
-                        <li>Traut TW, Jones ME. Uracil metabolism--UMP synthesis from orotic acid or uridine and conversion of uracil to beta-alanine: enzymes and cDNAs. <em>Prog Nucleic Acids Res Mol Biol 1996;53:1-78.</em>
-                        <li>Moyer JD, Henderson JF. Salvage of uridine in the mouse. Effect of uridine phosphorylase pre-treatment. <em>Biochem Pharmacol 1985;34:101–105.</em>
-                        <li>Löffler M, Fairbanks LD, Zameitat E, Marinaki AM, Simmonds HA. Pyrimidine pathways in health and disease. <em>Trends Mol Med 2005;11(9):430-7.</em>
-                        <li>Fuke C, Krikorian SA, Couris RR. Coenzyme Q10: a review of essential functions and clinical trials. <em>US Pharmacist2000;28-41.</em>
-                        <li>Hodgson JM, Watts GF, Playford DA, Burke V, Croft KD. Coenzyme Q10 improves blood pressure and glycaemic control: a controlled trial in subjects with type 2 diabetes. <em>Eur J Clin Nutr 2002;56:1137-42.</em>
-                        <li>Langsjoen PH, Langsjoen AM. The clinical use of HMG CoA-reductase inhibitors and the associated depletion of coenzyme Q10. A review of animal and human publications. <em>Biofactors 2003;18:101-11.</em>
-                        <li>Vitetta L, Manuel R, Zhou JY, et al. The overarching influence of the gut microbiome on end-organ function: the role of live probiotic cultures. <em>Pharmaceuticals (Basel) 2014;7(9):954-89.</em>
-                        <li>Morelli L, Capurso L. FAO/WHO Guidelines on Probiotics: 10 years on. <em>J Clin Gastroenterol 2012;46(1): S1-S2.</em>
-                        <li>Timmerman HM, Koning CJ, Mulder L, Rombouts FM, Beynen AC: Monostrain, multistrain and multispecies probiotics--A comparison of functionality and efficacy. <em>Int J Food Microbiol 2004;96:219-233.</em>
-                        <li>Vitetta L, Briskey D, Hayes E, Shing C, Peake J: A review of the pharmacobiotic regulation of gastrointestinal inflammation by probiotics, commensal bacteria and prebiotics. <em>Inflammopharmacology 2012;20:251-266.</em>
+                        <li>Carmel R. Efficacy and safety of fortification and supplementation with vitamin B12: biochemical and physiological effects. <em>Food Nutr Bull 2008;29:S177-S187.</em></li>
+                        <li>Nielsen, HM 2014, ‘Epithelial permeation and absorption mechanisms of biopharmaceuticals’, in <em>J das Neves & B Sarmento (eds), Mucosal Delivery of Biopharmaceuticals, Springer, New York, pp. 99-124.</em></li>
+                        <li>Laffleur F, Bernkop-Schnürch A. Strategies for improving drug delivery. <em>Future Medicine 2013;8:2061-75.</em></li>
+                        <li>Ansari Z. Homocysteine and mild cognitive impairment: are these the tools for early intervention in the dementia spectrum? <em>J Nutr health Aging 2016;20:155-60.</em></li>
+                        <li>Ganguly P, Alam SF. Role of homocysteine in the development of cardiovascular disease. <em>Nutr J 2015;14:6.</em></li>
+                        <li>Obeid R, Fedosov SN, Nexo E. Cobalamin coenzyme forms are not likely to be superior to cyano- and hydroxyl-cobalamin in prevention or treatment of cobalamin deficiency. <em>Mol Nutr Food Res 2015;59:1364-1372.</em></li>
+                        <li>Thakkar K, Billa G. Treatment of vitamin B12 deficiency – Methylcobalamine? Cyanocobalamine? Hydroxycobalamin? – clearing the confusion. <em>Eur J Clin Nutr 2015;69:1-2.</em></li>
 
                     </ol>
                     <hr>
