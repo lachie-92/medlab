3f1b55668c6a9e18a84db9f137afc2ebae918d22
LachlanWork <lachlan_young@medlab.co>
2016-08-22 11:45:47 +1000

    Careers Page

--- resources/views/pages/about/careers/partial/content.blade.php
+++ resources/views/pages/about/careers/partial/content.blade.php
@@ -22,7 +22,9 @@
                     <hr>
                     <h4>Positions Available</h4>
                     <a href="#RegulatoryAffairsProductManager">Regulatory Affairs Product Manager</a></br>
+                    <a href="#DigitalMarketingCoordinator">Digital Marketing Coordinator</a></br>
                     <a href="#ClientRelationshipManager">Client Relationship Manager</a></br>
+                    <a href="#PractitionerConsultantFieldRep">Practitioner Consultant - Field Rep, Gold Coast</a></br>
                     <a href="#PractitionerConsultantRepNSW">Practitioner Consultant Field Rep New South Wales</a></br>
                     <a href="#PractitionerConsultantRepSEQ">Practitioner Consultant Field Rep South East Queensland</a></br>
                     <a href="#PractitionerConsultantRepVIC">Practitioner Consultant Field Rep Victoria</a></br>
@@ -72,6 +74,53 @@
                     <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Regulatory Affairs Product Manager">recruit@medlab.co</a></p>
                     <hr>
 
+
+                    <a name="DigitalMarketingCoordinator"></a>
+                    <h3>Digital Marketing Coordinator</h3>
+                    <li>Full Time / Permanent</li>
+                    <li>Based in Alexandria, Sydney</li></br>
+                    <p>Due to continued growth, Medlab Clinical are currently seeking an enthusiastic, creative and passionate Digital Marketing Coordinator to join our Sales and Marketing Team.</p>
+
+                    <h4>The Opportunity</h4>
+                    <p>In this newly created role as the Digital Marketing Coordinator, you will report to the Sales and Marketing Manager where you will assist with the development and delivery of digital projects to the Australian and International market. This is an exciting opportunity to utilise your digital marketing and creative design experience to make this role your own.</p>
+                    <p>Duties and Responsibilities include:</p>
+                    <li>Using MailChimp, create and deliver regular eDM’s to create brand awareness, launch new products and increase sales of nutraceuticals.</li>
+                    <li>Develop reporting frameworks to monitor and track campaign performance, including the use of Google Analytics.</li>
+                    <li>Maintaining and developing website content and functionality.</li>
+                    <li>Create and manage engaging content for social media platforms.</li>
+                    <li>Report on social media metrics and develop strategies to improve engagement and conversion.</li>
+                    <li>Maximise brand awareness and identify areas for brand development.</li>
+                    <li>Create and develop relationships with bloggers and influencers.</li>
+                    <li>Contribute to the development of the annual marketing plan as required.</li>
+                    <li>Assist the marketing team with the coordination, execution and evaluation of promotional campaigns.</li></br>
+
+                    <h4>About You</h4>
+                    <p>To be successful in this role you will be able to demonstrate the following:</p>
+                    <li>Minimum 2 years experience in a digital marketing role.</li>
+                    <li>Ability to think creatively and have a passion for building a brand within the online social media space.</li>
+                    <li>Stay up to date with the latest technology advancements and social media trends.</li>
+                    <li>Ability to influence and engage across a diverse range of stakeholders.</li>
+                    <li>Exceptional attention to detail along with high level organizational skills with the ability to meet deadlines.</li>
+                    <li>Ability to work both collaboratively within a team and autonomously.</li>
+                    <li>Self motivated and proactive in bringing your ideas to life.</li>
+                    <li>Strong communication skills both written and verbal.</li>
+                    <li>Experience in using Google AdWords, Google Analytics and Facebook Ad Manager.</li>
+                    <li>Experience in using MailChimp, Adobe Photoshop/In Design.</li>
+                    <li>Genuine interest in the health and wellness sector.</li></br>
+
+                    <h4>Benefits</h4>
+                    <p>This is a fantastic opportunity to join a globally established business that has great plans for expansion. Here at Medlab we are committed to the development of staff and regularly promote from within.</p>
+                    <p>You will be joining a highly motivated team with an organisation who provides excellent employee benefits and incentives which include the following:</p>
+                    <li>Generous base salary</li>
+                    <li>A fun, collaborative team-working environment.</li>
+                    <li>Use of on-site gym facilities</li>
+                    <li>On-going training, education and mentoring.</li>
+                    <li>Career growth for the right person.</li>
+                    <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change…</p>
+                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Digital Marketing Coordinator">recruit@medlab.co</a></p>
+                    <hr>
+
+
                     <a name="ClientRelationshipManager"></a>
                     <h3>Client Relationship Manager</h3>
                     <li>Client Relationship Manager</li>
@@ -113,6 +162,50 @@
                     <li>Casual dress code</li></br>
                     <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change…</p>
                     <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Client Relationship Manager">recruit@medlab.co</a></p>
+                    <hr>
+
+
+                    <a name="PractitionerConsultantFieldRep"></a>
+                    <h3>Practitioner Consultant - Field Rep, Gold Coast</h3>
+                    <li>Full Time / Permanent</li>
+                    <li>Career Growth</li></br>
+                    <p>Due to a rapid increase in market share we are seeking a highly ambitious Practitioner Consultant Field Rep to join our extremely motivated and successful team, where we can provide you with ongoing career development within a rewarding and collaborative team culture.</p>
+
+                    <h4>Who are Medlab Clinical?</h4>
+                    <p>Medlab Clinical Ltd is an ASX listed certified biotechnology company, experienced in microbiome research that is being applied towards developing scientific-based pharmaceuticals and nutraceuticals. We pride ourselves on being an industry leader within this field and conduct our research within our purpose built PC2 accredited laboratory.</p>
+
+                    <h4>The Opportunity</h4>
+                    <p>As a passionate Field Rep, you will act as the interface between our company and healthcare practitioners, consultatively working to increase the visibility and awareness of Medlab's practitioner-only nutraceutical range within your territory, whilst meeting and exceeding sales targets.</p>
+                    <p>The role will involve face to face new business appointments with Practitioners to introduce Medlab’s range of products along with providing product education and training to practitioners. To be successful in this role you must possess strong communication and relationship building skills, whilst not being afraid of a challenge.</p>
+                    <p>Duties and Responsibilities include:</p>
+                    <li>Continually identify and develop new business opportunities.</li>
+                    <li>Increase product sales in existing and new clients through building strong relationships and the ongoing education and training of healthcare practitioners, either face to face or over the phone.</li>
+                    <li>Execute a business plan for your territory inline with meeting and exceeding key performance indicators and territory targets.</li>
+                    <li>Create and implement client incentive and retention strategies.</li>
+                    <li>Ensure the highest level of product and technical knowledge is attained and maintained.</li>
+                    <li>Provide technical support to practitioners over the phone.</li>
+                    <li>Plan and deliver product training to a range of healthcare modalities.</li>
+                    <li>Attend industry and association conferences and trade shows when required.</li></br>
+
+                    <h4>About You</h4>
+                    <p>To be successful in this role you will be able to demonstrate the following:</p>
+                    <li>You will ideally hold a naturopathic or similar complementary medicine qualification or have a pharmacy, nursing or science related qualification.</li>
+                    <li>Possess high level of interpersonal skills, with the ability to motivate and influence, creating strong relationships within the practitioner field.</li>
+                    <li>Proven track record in a sales environment along with commercial acumen.</li>
+                    <li>Self motivated and have a solutions based approach to tasks.</li>
+                    <li>Excellent understanding of the sales process and use of CRM system.</li>
+                    <li>Have a genuine passion for health and wellness with desire to succeed.</li>
+                    <li>Note: This position would also suit a person who is from a healthcare practitioner background looking for a change in career direction.</li></br>
+
+                    <h4>Benefits</h4>
+                    <p>This is a fantastic opportunity to join a globally established business that has great plans for expansion. Here at Medlab we are committed to the development of staff and regularly promote from within.</p>
+                    <p>You will be joining a highly motivated team with an organisation who provides excellent employee benefits and incentives which include the following:</p>
+                    <li>Generous base salary plus commission structure and allowances.</li>
+                    <li>A fun, collaborative team-working environment.</li>
+                    <li>On-going training, education and mentoring.</li>
+                    <li>Career growth for the right person.</li>
+                    <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change…</p>
+                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Practitioner Consultant - Field Rep, Gold Coast">recruit@medlab.co</a></p>
                     <hr>
 
 
