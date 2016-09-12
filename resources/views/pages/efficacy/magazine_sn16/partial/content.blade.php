d14605a9fb595a870b942b1270fdcdfc536a1526
Henry <henry.work@iinet.net.au>
2016-09-13 09:45:18 +1000

    add disclaimer on efficacy magazine

--- resources/views/pages/efficacy/magazine_sn16/partial/content.blade.php
+++ resources/views/pages/efficacy/magazine_sn16/partial/content.blade.php
@@ -14,9 +14,31 @@
                     Medlab Efficacy Magazine Edition 1
                 </div>
                 <div class="panel-body medlab_panel_content">
-
-
                     <iframe src="//cdn.flipsnack.com/widget/flipsnackwidget.html?hash=ftpfgj99v&bgcolor=EEEEEE&t=1473635435" width="100%" height="900pz" seamless="seamless" scrolling="no" frameBorder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+                    <br><br>
+                    <div class="container-fluid">
+                        <div class="well well-sm" style="text-align: justify;">
+                            <p><strong>DISCLAIMER:</strong></p>
+                            <p>EFFICACY MAGAZINE is published quartley by Medlab Clinical Ltd. </p>
+                            <p>
+                                It contains articles and content written by healthcare professionals and practitioners.
+                                Content is selected by the research team, with the aim to provide readers with credible
+                                and accurate information regarding Medlab products, research, and the nutraceutical industry.
+                            </p>
+                            <p>
+                                The information contained in this publication does not cover all possible uses, actions, precautions, side effects and interactions.
+                            </p>
+                            <p>
+                                We accept no liability for individual actions or omissions that are based on content found within this publication. No content may be reprinted, replicated or copied.
+                            </p>
+                            <p>	&copy;Copyright Medlab Clinical Ltd 2016</p>
+                            <p>
+                                - Not For Public Distribution
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
