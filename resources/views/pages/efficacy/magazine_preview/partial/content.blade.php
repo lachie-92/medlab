547e225864d8b90b7b540f2b05110e9fb925cd76
LachlanWork <lachlan_young@medlab.co>
2016-09-16 13:44:56 +1000

    Efficacy

--- resources/views/pages/efficacy/magazine_preview/partial/content.blade.php
+++ resources/views/pages/efficacy/magazine_preview/partial/content.blade.php
@@ -41,16 +41,12 @@
 
                                 <h1 style="text-align:center;">Medlab's New Efficacy Magazine</h1>
                                 <h4 style="color:grey; text-align:center;">A true integrative approach to medicine and health</h4>
-                                </br>
                                 <p style="text-align:center;">Issue 1: September - November 2016</p>
                                 <p style="text-align:center;">A Focus on the Gut-Brain Connection</p>
                                 </br>
                                 <p style="text-align:center;">We welcome you to view our new online Efficacy Magazine – much larger in size than previous newsletters, this Issue incorporates articles from health care practitioners within our community and key authorities and researchers – Professor Luis Vitetta and Dr Matthew Bambling. You will also find Medlab's latest research and company updates included.</p>
-                                </br>
                                 <p style="text-align:center;">Available exclusively online, we’ve also made the decision to move to an environmentally-friendly digital format. At Medlab Clinical, we pride ourseleves on being ethically responsible in everything we do, with Efficacy being no exception. This new digital format will also allow us to create more interactive content such as website and article links along with educational videos.</p>
-                                </br>
                                 <p style="text-align:center;">Our new Efficacy Magazine is compatible with microsoft, android and apple computers, tablets and phones.</p>
-                                </br>
                                 <p style="text-align:center;">Available to all healthcare practitioners when you <a href="/account/login">register or log in</a> today.</p>
 
 
