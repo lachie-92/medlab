9939447d074a126942c7e49181f03c9d126b1153
LachlanWork <lachlan_young@medlab.co>
2017-03-02 14:18:16 +1100

    Webinars Delted

--- resources/views/pages/education/webinars/NRGBiotic/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('app')
-
-@section('content')
-
-@include('pages.education.webinars.nrgbiotic.partial.breadcrumbs')
-@include('pages.education.webinars.nrgbiotic.partial.content')
-
-@endsection
