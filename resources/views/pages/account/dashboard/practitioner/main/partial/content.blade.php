c9860651ae7ee56f869273eab038385f004dbc63
LachlanWork <lachlan_young@medlab.co>
2017-03-03 13:39:04 +1100

    Efficacy

--- resources/views/pages/account/dashboard/practitioner/main/partial/content.blade.php
+++ resources/views/pages/account/dashboard/practitioner/main/partial/content.blade.php
@@ -64,19 +64,19 @@
 
 
                         <p>
-                            <a href="/efficacy/magazine_df16" target="_blank">
+                            <a href="/efficacy/magazine_mm17" target="_blank">
 
-                                <img alt="Medlab Efficacy Magazine Edition 1"
-                                     src="/img/efficacy/efficacy/yearinreview.jpg"
-                                     title="Medlab Efficacy Magazine Edition 1"
+                                <img alt="Medlab Efficacy Magazine Edition 3"
+                                     src="/img/efficacy/efficacy/EFFICACY Mag Web Banner 600x600 MAY17 v1[1].jpg"
+                                     title="Medlab Efficacy Magazine Edition 3"
                                      width="350px"
                                      class="img-responsive">
 
                             </a>
                         </p>
 
-                        <p>Medlab Efficacy Edition 2: December 2016 - February 2017</p>
-                        <p>Medlab's exclusive online magazine - <a href="/efficacy/magazine_sn16">view here</a></p>
+                        <p>Medlab Efficacy Edition 3: March 2017 - May 2017</p>
+                        <p>Medlab's exclusive online magazine - <a href="/efficacy/magazine_mm17">view here</a></p>
 
 
                     </div>
