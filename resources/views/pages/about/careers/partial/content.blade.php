a6a6fb93f05639b6c8d4187906c310d5c526c462
LachlanWork <lachlan_young@medlab.co>
2017-03-15 15:16:07 +1100

    Careers

--- resources/views/pages/about/careers/partial/content.blade.php
+++ resources/views/pages/about/careers/partial/content.blade.php
@@ -21,14 +21,55 @@
                     </p>
                     <hr>
                     <h4>Positions Available</h4>
-                    <!--<a href="#ClientRelationshipConsultant">Client Relationship Consultant</a></br>-->
-                    No positions currently available, for all expressions of interest please email <a href="mailto:recruit@medlab.co">recruit@medlab.co</a>
+                    <a href="#EATOCEO">EA to CEO</a></br>
 
+                    <!--
+                    No positions currently available, for all expressions of interest please email <a href="mailto:recruit@medlab.co">recruit@medlab.co</a>
+                    -->
 
 
                     <hr>
 
 
+                    <a name="EATOCEO"></a>
+                    <h3>EA to CEO</h3>
+                    <li>Full Time / Permanent</li>
+                    <li>Modern Alexandria Offices</li>
+                    <li>Maternity cover with opportunity to go permanent</li></br>
+
+                    <h4>The Opportunity</h4>
+                    <p>This is an excellent opportunity to work for a progressive company in the Health Sector as an Executive Assistant.</p>
+                    <p>This position reports directly to the CEO and is responsible for providing administration support to the CEO and the management team. This includes preparing, formatting and editing a range of documents, presentations and event co-ordination.</p>
+                    <p>Develop reporting frameworks to monitor and track campaign performance, including the use of Google Analytics.</p>
+                    <p>The role includes several specific administration responsibilities including but not limited to:</p>
+                    <li>Provide day to day support to the CEO.</li>
+                    <li>Diary management</li>
+                    <li>Coordination of high level meetings</li>
+                    <li>Coordination of all Company travel</li>
+                    <li>Office management</li></br>
+
+                    <h4>About You</h4>
+                    <p>To be successful in this role you will be able to demonstrate the following:</p>
+                    <li>Extensive experience in high level executive support</li>
+                    <li>Strong written communication skills and well developed interpersonal skills</li>
+                    <li>High attention to detail and ability to maintain confidentiality</li>
+                    <li>Strong organisational skills</li>
+                    <li>A can-do attitude</li>
+                    <li>Excellent time management skills and the ability to prioritise</li>
+                    <li>Intermediate/Advanced Microsoft Office skills</li>
+                    <li>Preferably experienced in Apple software - Keynote, Pages & Numbers.</li>
+                    <li>Short hand experience will be highly desirable</li>
+                    <li>Drivers licence essential</li></br>
+
+                    <h4>Benefits</h4>
+                    <p>You will be joining a highly motivated team with an organisation that provides excellent employee benefits and incentives which include the following:</p>
+                    <li>Genuine opportunities to develop your career</li>
+                    <li>Fun and friendly environment</li>
+                    <li>Use of on-site gym facilities  </li>
+                    <li>Onsite parking</li>
+                    <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change.....</p>
+                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - EA to CEO">recruit@medlab.co</a></p>
+                    <hr>
 
 
 <!--
