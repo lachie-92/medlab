61af125f36f75c63ffa2431125a96531acd8c30a
Andy Soell <andy@mettleup.com>
2017-03-27 10:45:31 +0200

    finish Care Plan view

---
+++ resources/views/pages/account/careplan/patient/view/index.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+
+@include('pages.account.dashboard.patient.main.partial.breadcrumbs')
+@include('pages.account.careplan.patient.edit.partial.page1')
+@include('pages.account.careplan.patient.edit.partial.page2')
+@include('pages.account.careplan.patient.edit.partial.page3')
+@include('pages.account.careplan.patient.edit.partial.page4')
+@endsection
