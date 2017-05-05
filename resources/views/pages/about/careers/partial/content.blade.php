7ab618e68e6b99973e357d081bee04e9932320db
LachlanWork <lachlan_young@medlab.co>
2017-05-05 13:51:57 +1000

    Careers

--- resources/views/pages/about/careers/partial/content.blade.php
+++ resources/views/pages/about/careers/partial/content.blade.php
@@ -21,8 +21,9 @@
                     </p>
                     <hr>
                     <h4>Positions Available</h4>
-                    <!--<a href="#CTO">Clinical Trial Coordinator</a></br>-->
                     <a href="#ClientRelationshipConsultant">Client Relationship Consultant</a></br>
+                    <a href="#PractitionerConsultantFieldRep">Practitioner Consultant Field Rep - South Perth, WA</a></br>
+
 
 
 
@@ -252,19 +253,21 @@
                     <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Client Relationship Consultant">recruit@medlab.co</a></p>
                     <hr>
 
-                    <!--
+
                     <a name="PractitionerConsultantFieldRep"></a>
-                    <h3>Practitioner Consultant Field Rep Gold Coast</h3>
-                    <li>Full Time / Permanent</li>
-                    <li>Career Growth</li></br>
-                    <p>Due to a rapid increase in market share we are seeking a highly ambitious Practitioner Consultant Field Rep to join our extremely motivated and successful team, where we can provide you with ongoing career development within a rewarding and collaborative team culture.</p>
+                    <h3>Practitioner Consultant Field – Field Sales Representative WA</h3>
+                    <li>Permanent / Part Time (3 days per week)</li>
+                    <li>Opportunity to work with a leading Biotechnology and Medical Research Company</li>
+                    <li>South Perth, WA</li></br>
+
+                    <p>Due to a rapid expansion, we are seeking a highly ambitious Practitioner Consultant to join our extremely successful sales team, where we can provide you with ongoing career development within a rewarding and collaborative team culture.</p>
 
                     <h4>Who are Medlab Clinical?</h4>
                     <p>Medlab Clinical Ltd is an ASX listed certified biotechnology company, experienced in microbiome research that is being applied towards developing scientific-based pharmaceuticals and nutraceuticals. We pride ourselves on being an industry leader within this field and conduct our research within our purpose built PC2 accredited laboratory.</p>
 
                     <h4>The Opportunity</h4>
-                    <p>As a passionate Field Rep, you will act as the interface between our company and healthcare practitioners, consultatively working to increase the visibility and awareness of Medlab's practitioner-only nutraceutical range within your territory, whilst meeting and exceeding sales targets.</p>
-                    <p>The role will involve face to face new business appointments with Practitioners to introduce Medlab’s range of products along with providing product education and training to practitioners. To be successful in this role you must possess strong communication and relationship building skills, whilst not being afraid of a challenge.</p>
+                    <p>As a passionate Practitioner Consultant, you will be the face between our company and healthcare practitioners, consultatively working to increase the visibility and awareness of Medlab's practitioner-only nutraceutical range within your territory whilst meeting and exceeding sales targets.</p>
+                    <p>The role will be a combination of business development over the phone, face to face appointments and product training to practitioners, therefore you must possess strong communication and relationship building skills, whilst not being afraid of a challenge.</p>
                     <p>Duties and Responsibilities include:</p>
                     <li>Continually identify and develop new business opportunities.</li>
                     <li>Increase product sales in existing and new clients through building strong relationships and the ongoing education and training of healthcare practitioners, either face to face or over the phone.</li>
@@ -283,7 +286,7 @@
                     <li>Self motivated and have a solutions based approach to tasks.</li>
                     <li>Excellent understanding of the sales process and use of CRM system.</li>
                     <li>Have a genuine passion for health and wellness with desire to succeed.</li>
-                    <li>Note: This position would also suit a person who is from a healthcare practitioner background looking for a change in career direction.</li></br>
+                    Note: This position would also suit a person who is from a healthcare practitioner background looking for a change in career direction.</br>
 
                     <h4>Benefits</h4>
                     <p>This is a fantastic opportunity to join a globally established business that has great plans for expansion. Here at Medlab we are committed to the development of staff and regularly promote from within.</p>
@@ -293,11 +296,11 @@
                     <li>On-going training, education and mentoring.</li>
                     <li>Career growth for the right person.</li>
                     <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change…</p>
-                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Practitioner Consultant - Field Rep, Gold Coast">recruit@medlab.co</a></p>
+                    <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Practitioner Consultant - Field Rep, WA">recruit@medlab.co</a></p>
                     <hr>
 
 
-                    -->
+
 
 
 
