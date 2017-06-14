da7df00d683f86adc015493d6e601ecaf7c631b3
LachlanWork <lachlan_young@medlab.co>
2017-06-14 11:03:21 +1000

    Tracking Code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,6 +22,9 @@
     <!-- App CSS -->
     <link rel="stylesheet" href="/css/app.css">
 
+    <!-- AutoPilot Tracking Code-->
+    <script type="text/javascript">(function(o){var b="https://api.autopilothq.com/anywhere/",t="1538831e17534a3ba1cd0c82ef9354e0ba643fcc956741f9862215872f3a93ff",a=window.AutopilotAnywhere={_runQueue:[],run:function(){this._runQueue.push(arguments);}},c=encodeURIComponent,s="SCRIPT",d=document,l=d.getElementsByTagName(s)[0],p="t="+c(d.title||"")+"&u="+c(d.location.href||"")+"&r="+c(d.referrer||""),j="text/javascript",z,y;if(!window.Autopilot) window.Autopilot=a;if(o.app) p="devmode=true&"+p;z=function(src,asy){var e=d.createElement(s);e.src=src;e.type=j;e.async=asy;l.parentNode.insertBefore(e,l);};if(!o.noaa){z(b+"aa/"+t+'?'+p,false)};y=function(){z(b+t+'?'+p,true);};if(window.attachEvent){window.attachEvent("onload",y);}else{window.addEventListener("load",y,false);}})({});</script>
+
 </head>
 
 <body>
