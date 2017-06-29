de36e7998ae7ae18730507b6b20bdac0cee3782b
LachlanWork <lachlan_young@medlab.co>
2017-06-29 10:29:00 +1000

    Products

--- resources/views/pages/nutraceuticals/developement/partial/breadcrumbs.blade.php
+++ resources/views/pages/nutraceuticals/developement/partial/breadcrumbs.blade.php
@@ -7,7 +7,6 @@
     <div class="container">
         <ol class="breadcrumb medlab_breadcrumbs">
             <li><a class="medlab_breadcrumbs_link" href="/">Home</a></li>
-            <li><a class="medlab_breadcrumbs_link" href="/nutraceuticals">Products</a></li>
             <li class="active medlab_breadcrumbs_text">Products In Development</li>
         </ol>
     </div>
