e0221688c971dec8e5039acbd42800722d63106e
LachlanWork <lachlan_young@medlab.co>
2017-08-30 15:05:49 +1000

    Careers

--- resources/views/pages/about/careers/partial/content.blade.php
+++ resources/views/pages/about/careers/partial/content.blade.php
@@ -23,7 +23,6 @@
                     <h4>Positions Available</h4>
 
 
-                    <a href="#MarketingOfficer">Marketing Officer</a></br>
                     <a href="#salesandmarketingassistant">Sales & Marketing Assistant</a></br>
                     <a href="#ClientRelationshipConsultant">Client Relationship Consultant</a></br>
                     <a href="#PracVIC">Practitioner Consultant – Field Sales Representative - Melbourne</a></br>
@@ -133,7 +132,7 @@
                     <p>This is an exceptional opportunity to be part of an expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change.....</p>
                     <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Clinical Trial Coordinator">recruit@medlab.co</a></p>
                     <hr>
-                    -->
+
 
 
 
@@ -187,6 +186,7 @@
                     <p>This is an exceptional opportunity to be part of an ASX listed, expanding global company, who are dedicated to improving health outcomes in chronic disease. Be part of the change…</p>
                     <p>Apply by emailing your cover letter and resume to <a href="mailto:recruit@medlab.co?Subject=Job Application - Marketing Officer">recruit@medlab.co</a></p>
                     <hr>
+                    -->
 
                     <a name="salesandmarketingassistant"></a>
                     <h3>Sales and Marketing Assistant</h3>
