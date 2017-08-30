857b5f6c29654c4cebef8747556f4591e757486f
Andy Soell <andy@mettleup.com>
2017-08-30 10:49:36 +0200

    consolidate About Us pages

--- resources/views/pages/about/about-medlab/index.blade.php
+++ resources/views/pages/about/about-medlab/index.blade.php
@@ -4,5 +4,7 @@
 
 @include('pages.about.about-medlab.partial.breadcrumbs')
 @include('pages.about.about-medlab.partial.content')
+@include('pages.about.who-we-are.partial.content')
+@include('pages.about.our-past.partial.content')
 
 @endsection
