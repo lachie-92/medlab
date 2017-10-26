1e429bd687f61fb4092fd312b22cd95ea46bf601
LachlanWork <lachlan_young@medlab.co>
2017-10-26 15:43:46 +1100

    Dropbox

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -462,7 +462,7 @@ Route::get('/about/careers', function () {
     return view('pages.about.careers.index');
 });
 
-Route::get('/ropboxfs0qd6txxt64', function () {
+Route::get('/dropboxfs0qd6txxt64', function () {
     return view('pages.dropbox.index');
 });
 
