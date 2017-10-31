587f9125c610b536d4e9e1a84136e49152178a27
Henry <henry.work@iinet.net.au>
2017-11-01 09:45:17 +1100

    nanabis page

--- resources/views/pages/research/sas/nanabis/partial/content.blade.php
+++ resources/views/pages/research/sas/nanabis/partial/content.blade.php
@@ -12,50 +12,98 @@
                     EMS Nanabis
                 </div>
                 <div class="panel-body medlab_panel_content">
-                    <div class="well">
+                    <div class="col-sm-12">
+                        <div class="well">
+                            <p>
+                                <b>
+                                    The purpose of this webpage is to educate and inform healthcare professionals about NanaBis.
+                                </b>
+                            </p>
+                            <p>
+                                <b>
+                                    Emergency medical support is offered to healthcare professionals on these numbers:
+                                </b>
+                            </p>
+                            <p>
+                                <b>
+                                    Within Australia: 1300 943 467<br>
+                                    Outside Australia: +61 2 8203 9523
+                                </b>
+                            </p>
+                            <p>
+                                <b>
+                                    If you are a patient and you believe you are having an adverse reaction you MUST immediately
+                                    call your Doctor or have a friend or family member drive you to the closest Hospital emergency
+                                    room.
+                                </b>
+                            </p>
+                        </div>
+
                         <p>
-                            <b>
-                                The purpose of this webpage is to educate and inform healthcare professionals about NanaBis.
-                            </b>
+                            NanaBis is an investigative new product containing 1:1 ratios of
+                            <a href="https://en.wikipedia.org/wiki/Cannabidiol" target="_blank">Cannabidiol</a> (CBD) and
+                            <a href="https://en.wikipedia.org/wiki/Tetrahydrocannabinol" target="_blank">delta-9-trethydrocannabinol</a>
+                            (THC), delivery in a sub micron spray to the <a href="https://en.wikipedia.org/wiki/Buccal_administration" target="_blank">oro-buccal membrane</a>.
+                            <a href="#" target="_blank">NanaBis PI can be found here</a>.
                         </p>
+
+                        <br>
+
                         <p>
-                            <b>
-                                Emergency medical support is offered to healthcare professionals on these numbers:<br><br>
-                                Within Australia: 1300 943 467<br>
-                                Outside Australia: +61 2 8203 9523
-                            </b>
+                            <span style="font-size: large;">
+                                <strong>NanaBis is a non-ARTG product currently in clinical trial and available under SAS.</strong>
+                            </span>
                         </p>
+
                         <p>
-                            <b>
-                                If you are a patient and you believe you are having an adverse reaction you MUST immediately
-                                call your Doctor or have a friend or family member drive you to the closest Hospital emergency
-                                room.
-                            </b>
+                            <img title="Ethics Approval" src="/img/research/SAS/ethics_approval.png"
+                                 alt="Ethics Approval" width="100%"/>
+                            <img title="Principal Investigator" src="/img/research/SAS/principal_investigator.png"
+                                 alt="Principal Investigator" width="100%"/>
+                        </p>
+
+                        <p>
+                            NanaBis is being studied as both an adjutant and a replacement to opioid therapy in advanced
+                            cancer pain. <a href="https://www.anzctr.org.au/Trial/Registration/TrialReview.aspx?ACTRN=12617001480370" target="_blank">ANZCTR listing is available here</a>.
+                        </p>
+
+                        <p>
+                            The consumer medical information <a href="#" target="_blank">(CMI) is available here</a>.
+                        </p>
+
+                        <p>
+                            Chances are you have come to this page because a patient presented to you has a MED IDENT
+                            bracelet on his or her wrist. A bracelet is provided with the investigational product to
+                            every patient, and its strongly recommended that the patient wear these at all times.
+                            Should the patient misplace or damage their bracelet, Medlab will be happy to replace at no charge.
+                        </p>
+
+                        <p>
+                            These bracelets look like this:
                         </p>
                     </div>
 
-                    <p>
-                        NanaBis is an investigative new product containing 1:1 ratios of
-                        <a href="https://en.wikipedia.org/wiki/Cannabidiol" target="_blank">Cannabidiol</a> (CBD) and
-                        <a href="https://en.wikipedia.org/wiki/Tetrahydrocannabinol" target="_blank">delta-9-trethydrocannabinol</a>
-                        (THC), delivery in a sub micron spray to the <a href="https://en.wikipedia.org/wiki/Buccal_administration" target="_blank">oro-buccal membrane</a>.
-                        <a href="#" target="_blank">NanaBis PI can be found here</a>.
-                    </p>
+                    <div class="col-sm-6 col-sm-offset-0 col-xs-8 col-xs-offset-2">
+                        <img title="Bracelet part one" src="/img/research/SAS/bracelet_part_one.jpeg"
+                             alt="Bracelet part one" width="100%"/>
+                    </div>
+                    <div class="col-sm-6 col-sm-offset-0 col-xs-8 col-xs-offset-2">
+                        <img title="Bracelet part two" src="/img/research/SAS/bracelet_part_two.jpeg"
+                             alt="Bracelet part two" width="100%"/>
+                    </div>
 
-                    <br>
+                    <div class="col-sm-12">
+                        <br>
+                        <p>
+                            <span style="font-size: large;">
+                                <strong>Access to medicinal cannabis products: steps to using access schemes.</strong>
+                            </span>
+                        </p>
+                        <p>
+                            <a href="https://www.tga.gov.au/access-medicinal-cannabis-products-steps-using-access-schemes" target="_blank">https://www.tga.gov.au/access-medicinal-cannabis-products-steps-using-access-schemes</a>
+                        </p>
+                    </div>
 
-                    <p style="text-align: justify;">
-                        <span style="font-size: medium;">
-                            <strong>NanaBis is a non-ARTG product currently in clinical trial and available under SAS.</strong>
-                        </span>
-                    </p>
-
-                    <p>
-                        <img title="Nanabis Ethics Approval" src="/img/research/nanabis_ethics_approval.png"
-                             alt="Nanabis Ethics Approval"/>
-                        <img title="Nanabis Principal Investigator" src="/img/research/nanabis_principal_investigator.png"
-                             alt="Nanabis Principal Investigator"/>
-                    </p>
                 </div>
             </div>
 
