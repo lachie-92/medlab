8aeb574221c98ff2b7cd999fdeb17608b1a1a18e
Henry <henry.work@iinet.net.au>
2017-11-01 09:07:20 +1100

    nanabis page partially done

--- resources/views/pages/research/sas/nanabis/partial/content.blade.php
+++ resources/views/pages/research/sas/nanabis/partial/content.blade.php
@@ -7,10 +7,57 @@
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
 
+            <div class="panel panel-primary medlab_panel">
+                <div class="panel-heading medlab_panel_title">
+                    EMS Nanabis
+                </div>
+                <div class="panel-body medlab_panel_content">
+                    <div class="well">
+                        <p>
+                            <b>
+                                The purpose of this webpage is to educate and inform healthcare professionals about NanaBis.
+                            </b>
+                        </p>
+                        <p>
+                            <b>
+                                Emergency medical support is offered to healthcare professionals on these numbers:<br><br>
+                                Within Australia: 1300 943 467<br>
+                                Outside Australia: +61 2 8203 9523
+                            </b>
+                        </p>
+                        <p>
+                            <b>
+                                If you are a patient and you believe you are having an adverse reaction you MUST immediately
+                                call your Doctor or have a friend or family member drive you to the closest Hospital emergency
+                                room.
+                            </b>
+                        </p>
+                    </div>
 
+                    <p>
+                        NanaBis is an investigative new product containing 1:1 ratios of
+                        <a href="https://en.wikipedia.org/wiki/Cannabidiol" target="_blank">Cannabidiol</a> (CBD) and
+                        <a href="https://en.wikipedia.org/wiki/Tetrahydrocannabinol" target="_blank">delta-9-trethydrocannabinol</a>
+                        (THC), delivery in a sub micron spray to the <a href="https://en.wikipedia.org/wiki/Buccal_administration" target="_blank">oro-buccal membrane</a>.
+                        <a href="#" target="_blank">NanaBis PI can be found here</a>.
+                    </p>
 
+                    <br>
 
+                    <p style="text-align: justify;">
+                        <span style="font-size: medium;">
+                            <strong>NanaBis is a non-ARTG product currently in clinical trial and available under SAS.</strong>
+                        </span>
+                    </p>
 
+                    <p>
+                        <img title="Nanabis Ethics Approval" src="/img/research/nanabis_ethics_approval.png"
+                             alt="Nanabis Ethics Approval"/>
+                        <img title="Nanabis Principal Investigator" src="/img/research/nanabis_principal_investigator.png"
+                             alt="Nanabis Principal Investigator"/>
+                    </p>
+                </div>
+            </div>
 
         </div>
     </div>
