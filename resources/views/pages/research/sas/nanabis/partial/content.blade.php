02cafb2afd2f0b32e842575e299f0e9a36899d9d
LachlanWork <lachlan_young@medlab.co>
2017-11-01 14:04:43 +1100

    Nanabis Page

--- resources/views/pages/research/sas/nanabis/partial/content.blade.php
+++ resources/views/pages/research/sas/nanabis/partial/content.blade.php
@@ -14,29 +14,32 @@
                 <div class="panel-body medlab_panel_content">
                     <div class="col-sm-12">
                         <div class="well">
-                            <p>
+
+                            <h1 class="text-center">
                                 <b>
                                     The purpose of this webpage is to educate and inform healthcare professionals about NanaBis.
                                 </b>
-                            </p>
-                            <p>
+                            </h1>
+                            <hr>
+                            <h4>
                                 <b>
                                     Emergency medical support is offered to healthcare professionals on these numbers:
                                 </b>
-                            </p>
-                            <p>
+                            </h4>
+                            <h3>
                                 <b>
-                                    Within Australia: 1300 943 467<br>
-                                    Outside Australia: +61 2 8203 9523
+                                    Within Australia: <a href="1300 943 467">1300 943 467</a><br>
+                                    Outside Australia: <a href="+61 2 8203 9523">+61 2 8203 9523</a>
                                 </b>
-                            </p>
-                            <p>
+                            </h3>
+                            <hr>
+                            <h4>
                                 <b>
                                     If you are a patient and you believe you are having an adverse reaction you MUST immediately
-                                    call your Doctor or have a friend or family member drive you to the closest Hospital emergency
+                                    call your Doctor or have a friend or family member drive you to the closest hospital emergency
                                     room.
                                 </b>
-                            </p>
+                            </h4>
                         </div>
 
                         <p>
@@ -44,8 +47,10 @@
                             <a href="https://en.wikipedia.org/wiki/Cannabidiol" target="_blank">Cannabidiol</a> (CBD) and
                             <a href="https://en.wikipedia.org/wiki/Tetrahydrocannabinol" target="_blank">delta-9-trethydrocannabinol</a>
                             (THC), delivery in a sub micron spray to the <a href="https://en.wikipedia.org/wiki/Buccal_administration" target="_blank">oro-buccal membrane</a>.
-                            <a href="#" target="_blank">NanaBis PI can be found here</a>.
+
                         </p>
+
+                        <a href="#" target="_blank">NanaBis product information can be found here</a>.
 
                         <br>
 
@@ -56,12 +61,14 @@
                         </p>
 
                         <p>
-                            <img title="Ethics Approval" src="/img/research/SAS/ethics_approval.png"
+                            <img title="Ethics Approval" src="/img/research/SAS/ethics.png"
                                  alt="Ethics Approval" width="100%"/>
-                            <img title="Principal Investigator" src="/img/research/SAS/principal_investigator.png"
+                            <br><br><br>
+                            <img title="Principal Investigator" src="/img/research/SAS/investigator.png"
                                  alt="Principal Investigator" width="100%"/>
                         </p>
 
+                        <br>
                         <p>
                             NanaBis is being studied as both an adjutant and a replacement to opioid therapy in advanced
                             cancer pain. <a href="https://www.anzctr.org.au/Trial/Registration/TrialReview.aspx?ACTRN=12617001480370" target="_blank">ANZCTR listing is available here</a>.
@@ -92,6 +99,7 @@
                              alt="Bracelet part two" width="100%"/>
                     </div>
 
+
                     <div class="col-sm-12">
                         <br>
                         <p>
@@ -99,8 +107,13 @@
                                 <strong>Access to medicinal cannabis products: steps to using access schemes.</strong>
                             </span>
                         </p>
+                        <a href="https://www.tga.gov.au/access-medicinal-cannabis-products-steps-using-access-schemes" target="_blank"><img title="Access To Medicinal Cannabis" src="/img/research/SAS/access.jpg"
+                             alt="Access To Medicinal Cannabis" width="100%"/></a>
                         <p>
                             <a href="https://www.tga.gov.au/access-medicinal-cannabis-products-steps-using-access-schemes" target="_blank">https://www.tga.gov.au/access-medicinal-cannabis-products-steps-using-access-schemes</a>
+                        </p>
+                        <p>
+                            <a href="https://www.tga.gov.au/access-medicinal-cannabis-products-steps-using-access-schemes#step2" target="_blank">https://www.tga.gov.au/access-medicinal-cannabis-products-steps-using-access-schemes#step2</a>
                         </p>
                     </div>
 
