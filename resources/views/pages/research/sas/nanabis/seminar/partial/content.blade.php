44ac6c1ff9c94c09b77848f7781e8e2b763541d3
LachlanWork <lachlan_young@medlab.co>
2018-01-18 11:00:08 +1100

    Nanabis Registration

--- resources/views/pages/research/sas/nanabis/seminar/partial/content.blade.php
+++ resources/views/pages/research/sas/nanabis/seminar/partial/content.blade.php
@@ -11,25 +11,41 @@
 
                 <div class="panel-body medlab_panel_content">
 
-                    <div id="wufoo-rd2ekh1pb8lp0">
-                        Fill out my <a href="https://medlabforms.wufoo.eu/forms/rd2ekh1pb8lp0">online form</a>.
-                    </div>
-                    <script type="text/javascript">var rd2ekh1pb8lp0;(function(d, t) {
-                            var s = d.createElement(t), options = {
-                                'userName':'medlabforms',
-                                'formHash':'rd2ekh1pb8lp0',
-                                'autoResize':true,
-                                'height':'565',
-                                'async':true,
-                                'host':'wufoo.eu',
-                                'header':'show',
-                                'ssl':true};
-                            s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.eu/scripts/embed/form.js';
-                            s.onload = s.onreadystatechange = function() {
-                                var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-                                try { rd2ekh1pb8lp0 = new WufooForm();rd2ekh1pb8lp0.initialize(options);rd2ekh1pb8lp0.display(); } catch (e) {}};
-                            var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-                        })(document, 'script');</script>
+
+
+
+                    <img alt="Nanabis"
+                         src="/img/news/bannernanabis2.png"
+                         title="Nanabis"
+                         width="100%"
+
+                         class="img-responsive center-block">
+
+                    <img alt="Nanabis"
+                         src="/img/news/bannernanabis.png"
+                         title="Nanabis"
+                         width="100%"
+
+                         class="img-responsive center-block">
+                    <hr>
+
+                    <iframe height="550"
+                            allowTransparency="true"
+                            frameborder="0"
+                            scrolling="no"
+                            style="width:100%;border:none"
+                            src="https://medlabforms.wufoo.eu/embed/rd2ekh1pb8lp0/">
+
+                    </iframe>
+                    <img alt="Nanabis"
+                         src="/img/news/blue.png"
+                         title="Nanabis"
+                         width="100%"
+
+                         class="img-responsive center-block">
+
+
+
 
 
                 </div>
